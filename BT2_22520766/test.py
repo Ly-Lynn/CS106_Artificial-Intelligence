0fadd4d8bfa47c641694c15c555efd0630144700
Lynnnnn <112313591+Lynn-is-me@users.noreply.github.com>
2024-03-30 01:11:41 +0700

    add file fast experiment and results

---
+++ BT2_22520766/test.py
@@ -0,0 +1,324 @@
+from level import Level
+from tqdm import tqdm
+import os
+import collections
+import numpy as np
+import heapq
+import time
+import numpy as np
+global posWalls, posGoals
+class PriorityQueue:
+    """Define a PriorityQueue data structure that will be used"""
+    def  __init__(self):
+        self.Heap = []
+        self.Count = 0
+        self.len = 0
+
+    def push(self, item, priority):
+        entry = (priority, self.Count, item)
+        heapq.heappush(self.Heap, entry)
+        self.Count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.Heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.Heap) == 0
+
+"""Load puzzles and define the rules of sokoban"""
+
+# def transferToGameState(layout):
+#     """Transfer the layout of initial puzzle"""
+#     layout = [x.replace('\n','') for x in layout]
+#     layout = [','.join(layout[i]) for i in range(len(layout))]
+#     layout = [x.split(',') for x in layout]
+#     maxColsNum = max([len(x) for x in layout])
+#     for irow in range(len(layout)):
+#         for icol in range(len(layout[irow])):
+#             if layout[irow][icol] == ' ': layout[irow][icol] = 0   # free space
+#             elif layout[irow][icol] == '#': layout[irow][icol] = 1 # wall
+#             elif layout[irow][icol] == '&': layout[irow][icol] = 2 # player
+#             elif layout[irow][icol] == 'B': layout[irow][icol] = 3 # box
+#             elif layout[irow][icol] == '.': layout[irow][icol] = 4 # goal
+#             elif layout[irow][icol] == 'X': layout[irow][icol] = 5 # box on goal
+#         colsNum = len(layout[irow])
+#         if colsNum < maxColsNum:
+#             layout[irow].extend([1 for _ in range(maxColsNum-colsNum)]) 
+
+    # print(layout)
+    # return np.array(layout)
+def transferToGameState2(layout, player_pos):
+    """Transfer the layout of initial puzzle"""
+    maxColsNum = max([len(x) for x in layout])
+    temp = np.ones((len(layout), maxColsNum))
+    for i, row in enumerate(layout):
+        for j, val in enumerate(row):
+            temp[i][j] = layout[i][j]
+
+    temp[player_pos[1]][player_pos[0]] = 2
+    return temp
+
+def PosOfPlayer(gameState):
+    """Return the position of agent"""
+    return tuple(np.argwhere(gameState == 2)[0]) # e.g. (2, 2)
+
+def PosOfBoxes(gameState):
+    """Return the positions of boxes"""
+    return tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5))) # e.g. ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5))
+
+def PosOfWalls(gameState):
+    """Return the positions of walls"""
+    return tuple(tuple(x) for x in np.argwhere(gameState == 1)) # e.g. like those above
+
+def PosOfGoals(gameState):
+    """Return the positions of goals"""
+    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5))) # e.g. like those above
+
+def isEndState(posBox):
+    """Check if all boxes are on the goals (i.e. pass the game)"""
+    return sorted(posBox) == sorted(posGoals)
+
+def isLegalAction(action, posPlayer, posBox):
+    """Check if the given action is legal"""
+    xPlayer, yPlayer = posPlayer
+    if action[-1].isupper(): # the move was a push
+        x1, y1 = xPlayer + 2 * action[0], yPlayer + 2 * action[1]
+    else:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+    return (x1, y1) not in posBox + posWalls
+
+def legalActions(posPlayer, posBox):
+    """Return all legal actions for the agent in the current game state"""
+    allActions = [[-1,0,'u','U'],[1,0,'d','D'],[0,-1,'l','L'],[0,1,'r','R']]
+    xPlayer, yPlayer = posPlayer
+    legalActions = []
+    for action in allActions:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+        if (x1, y1) in posBox: # the move was a push
+            action.pop(2) # drop the little letter
+        else:
+            action.pop(3) # drop the upper letter
+        if isLegalAction(action, posPlayer, posBox):
+            legalActions.append(action)
+        else: 
+            continue     
+
+    return tuple(tuple(x) for x in legalActions) # e.g. ((0, -1, 'l'), (0, 1, 'R'))
+
+def updateState(posPlayer, posBox, action):
+    """Return updated game state after an action is taken"""
+    xPlayer, yPlayer = posPlayer # the previous position of player
+    newPosPlayer = [xPlayer + action[0], yPlayer + action[1]] # the current position of player
+    posBox = [list(x) for x in posBox]
+    if action[-1].isupper(): # if pushing, update the position of box
+        posBox.remove(newPosPlayer)
+        posBox.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1]])
+    posBox = tuple(tuple(x) for x in posBox)
+    newPosPlayer = tuple(newPosPlayer)
+    return newPosPlayer, posBox
+
+def isFailed(posBox):
+    """This function used to observe if the state is potentially failed, then prune the search"""
+    rotatePattern = [[0,1,2,3,4,5,6,7,8],
+                    [2,5,8,1,4,7,0,3,6],
+                    [0,1,2,3,4,5,6,7,8][::-1],
+                    [2,5,8,1,4,7,0,3,6][::-1]]
+    flipPattern = [[2,1,0,5,4,3,8,7,6],
+                    [0,3,6,1,4,7,2,5,8],
+                    [2,1,0,5,4,3,8,7,6][::-1],
+                    [0,3,6,1,4,7,2,5,8][::-1]]
+    allPattern = rotatePattern + flipPattern
+
+    for box in posBox:
+        if box not in posGoals:
+            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1), 
+                    (box[0], box[1] - 1), (box[0], box[1]), (box[0], box[1] + 1), 
+                    (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
+            for pattern in allPattern:
+                newBoard = [board[i] for i in pattern]
+                if newBoard[1] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posBox: return True
+                elif newBoard[1] in posBox and newBoard[2] in posBox and newBoard[5] in posBox: return True
+                elif newBoard[1] in posBox and newBoard[6] in posBox and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls: return True
+    return False
+
+"""Implement all approcahes"""
+
+def cost(actions):
+    """A cost function"""
+    return len([x for x in actions if x.islower()])
+
+def uniformCostSearch(gameState):
+    """Implement uniformCostSearch approach"""
+    beginBox = PosOfBoxes(gameState)
+    beginPlayer = PosOfPlayer(gameState)
+
+    startState = (beginPlayer, beginBox)
+    frontier = PriorityQueue()
+    frontier.push([startState], 0)
+    exploredSet = set()
+    actions = PriorityQueue()
+    actions.push([0], 0)
+    temp = []
+    ### CODING FROM HERE ###
+    while frontier:
+        node = frontier.pop()
+        node_action = actions.pop()
+        
+        if isEndState(node[-1][-1]):
+            temp += node_action[1:]
+            break
+            
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                new_cost = cost(node_action[1:] + [action[-1]])
+                frontier.push(node + [(newPosPlayer, newPosBox)], new_cost)
+                actions.push(node_action + [action[-1]], new_cost)
+                
+    return temp
+
+
+def heuristic(posPlayer, posBox):
+    # print(posPlayer, posBox)
+    """A heuristic function to calculate the overall distance between the else boxes and the else goals"""
+    distance = 0
+    completes = set(posGoals) & set(posBox)
+    sortposBox = list(set(posBox).difference(completes))
+    sortposGoals = list(set(posGoals).difference(completes))
+    for i in range(len(sortposBox)):
+        distance += (abs(sortposBox[i][0] - sortposGoals[i][0])) + (abs(sortposBox[i][1] - sortposGoals[i][1]))
+    return distance
+
+def customHeuristic(posPlayer, posBox, startPos):
+    """Tie-breaking Heuristic"""
+    distance = 0
+    completes = set(posGoals) & set(posBox)
+    sortposBox = list(set(posBox).difference(completes))
+    sortposGoals = list(set(posGoals).difference(completes))
+    for i in range(len(sortposBox)):
+        # Calculate Manhattan distance for position of box and its goals
+        manhattan_distance = (abs(sortposBox[i][0] - sortposGoals[i][0])) + (abs(sortposBox[i][1] - sortposGoals[i][1]))
+
+        # In case of player do not hold any box, calculate distance between player and the nearest box
+        # manhattan_distance2 = (abs(sortposBox[i][0] - posPlayer[0])) + (abs(sortposBox[i][1] - posPlayer[1]))
+        
+        # Compute cross product from current box position to goal
+            # current box to goals 
+        dx1 = sortposBox[i][0] - sortposGoals[i][0]
+        dy1 = sortposBox[i][1] - sortposGoals[i][1]
+            # current box to start pos
+        dx3 = sortposBox[i][0] - startPos[i][0]
+        dy3 = sortposBox[i][1] - startPos[i][1]
+
+        cross_product = abs((dx1 * dy3) - (dx3 * dy1))
+        
+        # Compute cross product from start box position to goal
+        # dx4 = startPos[i][0] - sortposGoals[i][0]
+        # dy4 = startPos[i][1] - sortposGoals[i][1]
+        # cross_product_start = abs((dx4 * dy2) - (dx2 * dy4)) + abs((dx4 * dy3) - (dx3 * dy4))
+        
+        # Add cross products to the distance
+        distance += manhattan_distance  + cross_product * 0.001 
+    return distance
+
+
+def aStarSearch(gameState):
+    """Implement aStarSearch approach"""
+    # start =  time.time()
+    beginBox = PosOfBoxes(gameState)
+    beginPlayer = PosOfPlayer(gameState)
+    temp = []
+    start_state = (beginPlayer, beginBox)
+    frontier = PriorityQueue()
+    frontier.push([start_state], heuristic(beginPlayer, beginBox))
+    exploredSet = set()
+    actions = PriorityQueue()
+    actions.push([0], heuristic(beginPlayer, start_state[1]))
+    while len(frontier.Heap) > 0:
+        node = frontier.pop()
+        node_action = actions.pop()
+        if isEndState(node[-1][-1]):
+            temp += node_action[1:]
+            break
+
+        ### CONTINUE YOUR CODE FROM HERE
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                new_cost = cost(node_action[1:] + [action[-1]]) + heuristic(newPosPlayer, newPosBox)
+                frontier.push(node + [(newPosPlayer, newPosBox)], new_cost)
+                actions.push(node_action + [action[-1]], new_cost)
+    return temp
+    # end =  time.time()
+
+def aStarSearchCustom(gameState):
+    """Implement aStarSearch approach"""
+    # start =  time.time()
+    beginBox = PosOfBoxes(gameState)
+    beginPlayer = PosOfPlayer(gameState)
+    temp = []
+    start_state = (beginPlayer, beginBox)
+    frontier = PriorityQueue()
+    frontier.push([start_state], customHeuristic(beginPlayer, beginBox, beginBox))
+    exploredSet = set()
+    actions = PriorityQueue()
+    actions.push([0], customHeuristic(beginPlayer, start_state[1], beginBox))
+    while len(frontier.Heap) > 0:
+        node = frontier.pop()
+        node_action = actions.pop()
+        if isEndState(node[-1][-1]):
+            temp += node_action[1:]
+            break
+
+        ### CONTINUE YOUR CODE FROM HERE
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                new_cost = cost(node_action[1:] + [action[-1]]) + customHeuristic(newPosPlayer, newPosBox, beginBox)
+                frontier.push(node + [(newPosPlayer, newPosBox)], new_cost)
+                actions.push(node_action + [action[-1]], new_cost)
+    return temp
+    # end =  time.time()
+
+times=[]
+
+# for i in [16]:
+for i in range(0,16):
+    # if (i + 1) == 5:
+    #     continue
+    index_level = i + 1
+    print("level: ", index_level)
+    level = Level(index_level)
+    game_state = transferToGameState2(level.structure[:-1], level.position_player)
+    posWalls = PosOfWalls(game_state)
+    posGoals = PosOfGoals(game_state)
+
+    time_start = time.time()
+    # result = uniformCostSearch(game_state)
+    # result = aStarSearch(game_state)
+    result = aStarSearchCustom(game_state)
+    time_end = time.time()
+    folder_strategy = os.path.join(r"C:\Users\thuyl\OneDrive\My documents\AI\CS106_Artificial-Intelligence\BT2_22520766\assets", f"sokobanSolver_CustomAstart")
+    if not os.path.exists(folder_strategy):
+        os.mkdir(folder_strategy)
+    output_file = os.path.join(folder_strategy, f"level{index_level}Solver.txt")
+    with open(output_file, 'w+') as solver_file:
+        for listitem in result:
+            solver_file.write('%s, ' % listitem)
+        solver_file.write(f'\n{len(result)}')
+        solver_file.write(f"\n{time_end - time_start}")
+        times.append(time_end - time_start)
+print(times, end='\n')
+    # print(str([1,2]))
